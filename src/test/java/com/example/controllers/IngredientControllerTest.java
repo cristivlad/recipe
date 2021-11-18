540c40b7d8e6d7f6f527926e1ffecc8bd9c78b9c
cristianvlad <cristian.vlad10@gmail.com>
2021-11-18 16:38:56 +0200

    added support for deleting, updating a recipe plus the view of ingredients of a particular recipe

---
+++ src/test/java/com/example/controllers/IngredientControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.controllers;
+
+import com.example.commands.RecipesCommand;
+import com.example.services.RecipeService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.startsWith;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class IngredientControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController ingredientController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void beforeEach() {
+        ingredientController = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    public void testListIngredients() throws Exception {
+        RecipesCommand recipesCommand = new RecipesCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipesCommand);
+
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
